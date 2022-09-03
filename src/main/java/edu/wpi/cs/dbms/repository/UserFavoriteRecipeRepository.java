fac386559269a46e15541758a64c2800731cd206
chernikodv <dimon.chr98@gmail.com>
2022-09-03 14:09:56 -0400

    Updated user favorite recipe repository.

--- src/main/java/edu/wpi/cs/dbms/repository/UserFavoriteRecipeRepository.java
+++ src/main/java/edu/wpi/cs/dbms/repository/UserFavoriteRecipeRepository.java
@@ -1,17 +1,22 @@
 package edu.wpi.cs.dbms.repository;
 
 import edu.wpi.cs.dbms.domain.dto.recipe.TrendingRecipe;
+import edu.wpi.cs.dbms.domain.entity.User;
 import edu.wpi.cs.dbms.domain.entity.UserFavoriteRecipe;
 import edu.wpi.cs.dbms.domain.entity.UserFavoriteRecipeId;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@org.springframework.stereotype.Repository
-public interface UserFavoriteRecipeRepository extends Repository<UserFavoriteRecipe, UserFavoriteRecipeId> {
+public interface UserFavoriteRecipeRepository extends JpaRepository<UserFavoriteRecipe, UserFavoriteRecipeId> {
+
+    @EntityGraph(attributePaths = {"recipe"})
+    Page<UserFavoriteRecipe> findAllByUserOrderByLikedOnAsc(User user, Pageable pageable);
 
     @Query(name = "findTrendingRecipes", nativeQuery = true)
     List<TrendingRecipe> findTrendingRecipes(LocalDateTime start, LocalDateTime end, Pageable pageable);
