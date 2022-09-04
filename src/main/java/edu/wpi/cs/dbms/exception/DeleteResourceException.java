61d13c838d7083fb658d66eb3bdf95ce152fe7c6
chernikodv <dimon.chr98@gmail.com>
2022-09-03 20:01:45 -0400

    Removed unused exception.

--- src/main/java/edu/wpi/cs/dbms/exception/DeleteResourceException.java
+++
@@ -1,8 +0,0 @@
-package edu.wpi.cs.dbms.exception;
-
-public class DeleteResourceException extends RuntimeException {
-
-    public DeleteResourceException(String message) {
-        super(message);
-    }
-}
